c6ba543017e586e7faff1c55b2c4d9aa6df9ebb7
PradeeppaPalanisamy <pradeeppapk05@gmail.com>
2025-01-02 22:22:54 +0530

    Useparams hook was executed

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,22 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Link } from "react-router-dom";
 
-import state from './usestate.jsx'
-import State from './usestate.jsx'
-import App from './App.jsx'
+// import state from './usestate.jsx'
+// import State from './usestate.jsx'
+// import App from './App.jsx'
+// import List from './list.jsx'
+// import Hooks from './hooks.jsx'
+
+// import App1 from './App1.jsx'
+
+import Appparam from './App_param.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    
+    {/* <List /> */}
+    {/* <Hooks /> */}
+    <Appparam />
   </StrictMode>,
 )
